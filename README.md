0722cb57bad179dd0ef53ab953dd6df54e8eb2ce
Arun Lokanatha <arunkumarl87@gmail.com>
2023-03-15 20:02:07 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # vpc-setup-sagemaker-training
+
+This sample repository shows how to setup up and use VPC with SageMaker Training. There are two branches - 
+
+
+This repo contains a CFN template to launch the AWS infrastructure necessary. The notebook [training-vpc.ipynb](training-vpc.ipynb) has the necessary cells to launch the CFN stack and runs a training job that accesses your data in s3. Finally, it also has instructions for cleanup to avoid unnecessary costs.
+
+To run the notebook without any errors, make sure the following IAM policies are attached to the notebook's execution role.
+
+- [NetworkAdministrator](https://console.aws.amazon.com/iam/home#/policies/arn:aws:iam::aws:policy/job-function/NetworkAdministrator)
+- [AmazonSageMakerFullAccess](https://console.aws.amazon.com/iam/home#/policies/arn:aws:iam::aws:policy/AmazonSageMakerFullAccess)
+- [AWSCloudFormationFullAccess](https://console.aws.amazon.com/iam/home#/policies/arn:aws:iam::aws:policy/AWSCloudFormationFullAccess)
+
---
+++ train.py
@@ -0,0 +1,37 @@
+# This is a dummy training script that simply lists files in your mount
+# target, prints inside a for loop and saves a dummy model into EFS (and S3)
+# Update this script to your model training script 
+import argparse
+import os
+import time
+
+
+def train(args):
+
+    print('args', args)
+    tmp = os.listdir(args.training_dir)
+    print ('listing training files', tmp)
+
+    # Run dummy training loop.
+    epochs = 5
+    for x in range(epochs):
+        print("\nRunning epoch {0}...".format(x))
+        time.sleep(2)
+        print("Completed epoch {0}.".format(x))
+
+    # At the end of the training loop, save model artifacts.
+    model_filename = args.model_dir+"/out.txt"
+    os.makedirs(os.path.dirname(model_filename), exist_ok=True)
+    with open(model_filename, "w") as f:
+        f.write("Writing a sample file out!!!")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--training-dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+    
+    args = parser.parse_args()
+
+    train(args)
